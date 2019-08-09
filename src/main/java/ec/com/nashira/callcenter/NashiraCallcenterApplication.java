e1f99a5a7b5cd657fde421aa9de5bfbf59b51e3d
Giova <giovanotti1906@hotmail.com>
2019-08-09 11:58:59 -0500

    Initial commit

---
+++ src/main/java/ec/com/nashira/callcenter/NashiraCallcenterApplication.java
@@ -0,0 +1,13 @@
+package ec.com.nashira.callcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NashiraCallcenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NashiraCallcenterApplication.class, args);
+	}
+
+}
